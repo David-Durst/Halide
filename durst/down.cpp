e79c1e171d1ec6e8c655c8ae791f4fb8709c366c
David Durst <davidbdurst@gmail.com>
2020-04-20 10:14:17 -0700

    adding down

--- durst/down.cpp
+++ durst/down.cpp
@@ -1,10 +1,10 @@
 // On linux, you can compile and run it like so:
-// g++ simple.cpp -g -I ../include -L ../bin -lHalide -lpthread -ldl -o simple -std=c++11
-// LD_LIBRARY_PATH=../bin ./simple
+// g++ down.cpp -g -I ../include -L ../bin -lHalide -lpthread -ldl -o down -std=c++11
+// LD_LIBRARY_PATH=../bin ./down
 
 // On os x:
-// g++ simple.cpp -g -I ../include -L ../bin -lHalide -o simple -std=c++11
-// DYLD_LIBRARY_PATH=../bin ./simple
+// g++ down.cpp -g -I ../include -L ../bin -lHalide -o down -std=c++11
+// DYLD_LIBRARY_PATH=../bin ./down
 
 // The only Halide header file you need is Halide.h. It includes all of Halide.
 #include "Halide.h"
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     // This program defines a single-stage imaging pipeline that
     // outputs a grayscale diagonal gradient.
 
-    Halide::Func gradient;
+    Halide::Func gradient, down;
 
     // Var objects are names to use as variables in the definition of
     // a Func. They have no meaning by themselves.
@@ -28,9 +28,13 @@ int main(int argc, char **argv) {
     gradient.bound(x, 0, 400);
     gradient.bound(y, 0, 400);
 
+    down(x,y) = gradient(2*x,2*y);
+
+    gradient.compute_at(gradient, x);
+
     Halide::Target target = Halide::get_host_target();
 
-    Halide::Module module = gradient.compile_to_module({}, "", target);
+    Halide::Module module = down.compile_to_module({}, "", target);
 
     std::cout << module << std::endl;
 
